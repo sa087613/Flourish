4a1e6f4fade2897a44aafe9c00ae038e00eb172d
Soph Aban <abanadorsophia@gmail.com>
2025-12-21 18:58:56 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,8 +29,8 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Gamja+Flower&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Gamja+Flower&display=swap" rel="stylesheet">
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}

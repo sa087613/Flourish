7b1b8cbc41e71b1d3f937c8a90ea5ce75d9fd4e0
Soph Aban <abanadorsophia@gmail.com>
2025-12-21 19:28:44 -0500

    Update resizable-navbar.tsx

--- components/resizable-navbar.tsx
+++ components/resizable-navbar.tsx
@@ -242,7 +242,7 @@ export const NavbarLogo = () => {
         width={35}
         height={35}
       />
-      <span className="text-stone-600 text-xl font-extrabold" style={{ fontFamily: "'Miniver', serif" }}>Flourish</span>
+      <span className="text-stone-600 text-xl font-extrabold" style={{ fontFamily: "'Story Script', serif" }}>Flourish</span>
     </Link>
   );
 };
